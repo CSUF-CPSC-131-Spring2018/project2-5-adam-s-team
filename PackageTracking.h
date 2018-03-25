f7370c62ffeefad933a92ecb2282cd56aa678055
Feldman0007 <35717802+Feldman0007@users.noreply.github.com>
2018-03-25 14:58:56 -0700

    Update PackageTracking.h

--- PackageTracking.h
+++ PackageTracking.h
@@ -1,3 +1,4 @@
+#pragma once
 //
 //  PackageTracking.h
 //  Project2
@@ -23,32 +24,32 @@ using namespace std;
 
 class PackageTracking {
 public:
-    PackageTracking(const string& strnum);
-    
-    void m_addUpdate( const string& status, const string& location, const time_t& timeUpdated); // add a new update
-    
-    bool m_moveBackward();//move iterator one step back in time; return false if not possible (true otherwise)
-    bool m_moveForward();//move iterator one step forward in time; return false if not possible (true otherwise)
+	PackageTracking(const string& strnum);
 
-    string m_getLocation( );//return the location of the current update
-    time_t m_getTime( );//return the time of the current update
-    string m_getStatus( );//return the status of the current update
-    int m_getNumofUpdate() const; // get the total numbers of shipping status updates
-    
-    bool m_setCurrent(const time_t& timeUpdated);//set current update to given time; return false if time is not found (true otherwise)
-    
-    void m_printPreviousUpdates();  //print all previous updates in the shipping chain from beginning, all the way up to (but not including) the current update you are viewing (may not be the most recent update).
-    void m_printFollowingUpdates();//print all updates from the current update you are viewing to the last update in the tracking chain.
-    void m_printFullTracking();//print all the status updates in the tracking chain.
-    
-	//read the full tracking chain from a file and follow the commands as specified in the file
-	//return false if there is an error reading file (true otherwise)
+	void m_addUpdate(const string& status, const string& location, const time_t& timeUpdated); // add a new update
+
+	bool m_moveBackward();//move iterator one step back in time; return false if not possible (true otherwise)
+	bool m_moveForward();//move iterator one step forward in time; return false if not possible (true otherwise)
+
+	string m_getLocation();//return the location of the current update
+	time_t m_getTime();//return the time of the current update
+	string m_getStatus();//return the status of the current update
+	int m_getNumofUpdate() const; // get the total numbers of shipping status updates
+
+	bool m_setCurrent(const time_t& timeUpdated);//set current update to given time; return false if time is not found (true otherwise)
+
+	void m_printPreviousUpdates();  //print all previous updates in the shipping chain from beginning, all the way up to (but not including) the current update you are viewing (may not be the most recent update).
+	void m_printFollowingUpdates();//print all updates from the current update you are viewing to the last update in the tracking chain.
+	void m_printFullTracking();//print all the status updates in the tracking chain.
+
+							   //read the full tracking chain from a file and follow the commands as specified in the file
+							   //return false if there is an error reading file (true otherwise)
 	bool m_readTrackingFile(string fileName);
-	    
-    
-private:
 
+private:
+	string trackingNumber;
+	list<ShippingStatus> tracker;
+	list<ShippingStatus>::iterator cursor;
 };
 
-#endif /* PackageTracking_h */
-
+#endif
